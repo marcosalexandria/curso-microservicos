5600b13b5ed12ee9b47706b307ccc68fd86e4bce
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-08-20 15:10:27 -0300

    Feign Client

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/feingnclients/WorkerFeingnClient.java
@@ -0,0 +1,18 @@
+package com.devsuperior.hrpayroll.feingnclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.devsuperior.hrpayroll.entities.Worker;
+
+@Component	//Componente gerenciado pelo spring, possivel injetar em outras classes
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeingnClient {
+	
+	//A mesma assinatura do método na classe de WorkerResourcer no projeto hr-worker
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id);
+}
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -10,29 +10,19 @@ import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feingnclients.WorkerFeingnClient;
 
 @Service
 public class PaymentService {
 	
-	//atribuido para workerHost a variavel hr-worker.host do application.properties
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
-	//dependencia do RestTemplate que está instanciado no AppConfig.restTemplate
+	//Implementação do inteface WorkerFeingnClient
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeingnClient workerFeingnClient;
 	
 	public Payment getPayment(Long workerId, Integer days) {
 		
-		Map<String, String> uriVariables = new HashMap<>();
 		
-		uriVariables.put("id", workerId.toString());
-		
-		//criando uma resuisição com restTemplate entre 2 sistemas diferentes
-		Worker worker = restTemplate.getForObject(
-				workerHost + "/workers/{id}", //caminho do host com a variavel entre chaves
-				Worker.class, 	//a classe que esperamos de retorno
-				uriVariables);	//os parametros que vão ficar no lugar das variaveis que estão entre chaves
+		Worker worker = workerFeingnClient.findById(workerId).getBody(); //pegando o corpo da requisição que vai vim do hr-worker
 		
 		Payment payment = new Payment();
 		payment.setName(worker.getName());
