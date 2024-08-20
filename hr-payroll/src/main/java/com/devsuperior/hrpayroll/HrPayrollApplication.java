f42534f255f2234a8c8908b3fd71c1970a474d81
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-08-20 17:11:35 -0300

    Ribbon balanceamento de carga

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "hr-worker") //para o ribbon funcionar e transformar o projeto em um cliente ribbon do projeto hr-worker
 @EnableFeignClients	//Para o Feing fucionar no projeto
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/feingnclients/WorkerFeingnClient.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/feingnclients/WorkerFeingnClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.devsuperior.hrpayroll.entities.Worker;
 
 @Component	//Componente gerenciado pelo spring, possivel injetar em outras classes
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeingnClient {
 	
 	//A mesma assinatura do método na classe de WorkerResourcer no projeto hr-worker
