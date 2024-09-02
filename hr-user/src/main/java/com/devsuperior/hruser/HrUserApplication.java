9106df47366f13125971b1968bf9a6f18a507b80
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-09-02 14:18:45 -0300

    configurando o projeto hr-user

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -2,7 +2,9 @@ package com.devsuperior.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient //Tornando um cliente eureka
 @SpringBootApplication
 public class HrUserApplication {
 
